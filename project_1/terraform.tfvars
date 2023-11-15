fa1ac7ec4829dfaf9d7893c1f048b6e57825eaf3
pyEdwin <cyredwin1@gmail.com>
2023-11-15 18:32:37 -0500

    add variables.tf and terraform.tfvars

---
+++ project_1/terraform.tfvars
@@ -0,0 +1,4 @@
+# Variable values
+region = "YOUR_REGION"
+access_key = "YOUR_ACCESS_KEY"
+secret_ket = "YOUR_SECRET_KEY"
---
+++ project_1/variables.tf
@@ -0,0 +1,12 @@
+# Variables
+variable "access_key" {
+    description = "Access key to the AWS console"
+}
+
+variable "secret_ket" {
+    description = "Secret key to the AWS console"
+}
+
+variable "region" {
+    description = "Location of your infrastructure"
+}
